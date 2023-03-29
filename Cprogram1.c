8eed3b0585e686f5984e095215b235f2c9f0d368
Anant gupta <91842337+Anant787@users.noreply.github.com>
2023-03-29 15:58:18 +0530

    Add files via upload
    Program7 is lex program to remove the comment from the C program.
    Cprogram1 is the file in which the comment and multi-comment code is write.
    and out.c is the file of output of lex program where it remove the comment and multi-comment from the C program

---
+++ Cprogram1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hey this sample Document :: ");
+	/*
+		this is multi-line comment
+	*/
+	
+	return 0;
+}
---
+++ out.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hey this sample Document :: ");
+	
+	
+	return 0;
+}
